fd2ce0e0d706d864c20b6e6a7bfd20fe58f6886a
wixmanipur <wixmanipur@gmail.com>
2024-10-21 11:24:32 +0530

    commit from wixmanipur

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-    return <dev>
-        <h1>Hello World</h1>
+  return (
+    <dev>
+      <h1>Hello, Amarjit i see your project</h1>
     </dev>
+  );
 }
 
-export default App
+export default App;
